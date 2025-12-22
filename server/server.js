4141c660ffae94aa9cb3367b6dcf078f14a77617
aditya-bhakat <bhakataditya0@gmail.com>
2025-12-23 01:02:54 +0530

    express mongo iniatilized

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from './controllers/clerkWebhooks.js';
+
+connectDB(); // Connect to the database
+const app = express();
+app.use(clerkMiddleware())
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS) for all routes
+app.use(express.json()); // Parse incoming JSON requests
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use("/api/clerk",clerkWebhooks)
+
+app.get('/', (req, res) => {
+    res.send('API is working...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
